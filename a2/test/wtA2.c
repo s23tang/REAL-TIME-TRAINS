b5dc8208f3b810084a7917b85a0ae9699eeade1a
g28wang <g28wang@ubuntu1204-006.student.cs.uwaterloo.ca>
2014-01-28 17:13:14 -0500

    ASST2: Added three kernel primatives to the code(Send, Receive, Reply) for inter-task communication.

--- a2/test/wtA2.c
+++ a2/test/wtA2.c
@@ -56,23 +56,29 @@ void Exit ( ) {
 //-----------------------------------------------------------------------------------------------
 //	Calling task sends a message to the destination task
 //-----------------------------------------------------------------------------------------------
-int Send ( Tid tid, char *message, int mslen, char *reply, int rplen ) {
+int Send ( int tid, char *message, int mslen, char *reply, int rplen ) {
 	asm("swi 6");
-} // Exit
+
+	return 0;
+} // Send
 
 //-----------------------------------------------------------------------------------------------
 //	Calling task receives a message from another task
 //-----------------------------------------------------------------------------------------------
-int Receive ( Tid *tid, char *message, int mslen ) {
+int Receive ( int *tid, char *message, int mslen ) {
 	asm("swi 7");
-} // Exit
+
+	return 0;
+} // Receive
 
 //-----------------------------------------------------------------------------------------------
 //	Calling task replys a message from a sender
 //-----------------------------------------------------------------------------------------------
-int Send ( Tid tid, char *reply, int rplen ) {
+int Reply ( int tid, char *reply, int rplen ) {
 	asm("swi 8");
-} // Exit
+
+	return 0;
+} // Reply
 
 
 
@@ -84,12 +90,32 @@ int Send ( Tid tid, char *reply, int rplen ) {
 //	Tasks that will be created by the first user task contains the following
 //-----------------------------------------------------------------------------------------------
 void theOtherTask(){
-	int myTid, myParentTid;
-	myTid = MyTid();
-	myParentTid = MyParentTid();
-	bwprintf(COM2, "MyTid: %d, MyParentTid: %d\n\r", myTid, myParentTid);
-	Pass();
-	bwprintf(COM2, "MyTid: %d, MyParentTid: %d\n\r", myTid, myParentTid);
+	char *msg = "Hello";
+	char reply[50];
+
+	bwprintf(COM2, "SENT\n\r");
+
+	Send(3, msg, 6, reply, 50);
+
+	bwprintf(COM2, "%c%c%c%c\n\r", reply[0],reply[1],reply[2],reply[3]);
+
+	bwprintf(COM2, "SENT\n\r");
+
+	Send(4, msg, 6, reply, 50);
+
+	bwprintf(COM2, "%c%c%c%c\n\r", reply[0],reply[1],reply[2],reply[3]);
+	Exit();
+} // theOtherTask
+
+void theOtherTask2(){
+	int sender;
+	char msg[50];
+	char *reply = "Fuck";
+	Receive( &sender, msg, 50);
+
+	bwprintf(COM2, "%c%c%c%c%c\n\r", msg[0],msg[1],msg[2],msg[3],msg[4]);
+	Reply(sender, reply, 5);
+
 	Exit();
 } // theOtherTask
 
@@ -98,17 +124,19 @@ void theOtherTask(){
 //	First user task that will be placed by the kernel into the priority queue
 //-----------------------------------------------------------------------------------------------
 void firstUserTask(){
-	void (*otherTask)();
+	void (*otherTask)();    //1st task for send
 	otherTask = theOtherTask;
+	void (*otherTask2)();   //2nd task for receive
+	otherTask2 = theOtherTask2;
 	//Create four instances 
 	unsigned int retVal;
 	retVal = Create(2, otherTask);	  //instances with lower priority
 	bwprintf(COM2, "Created: %d\n\r", retVal);
-	retVal = Create(2, otherTask);
+
+	retVal = Create(2, otherTask2);	  //instances with lower priority
 	bwprintf(COM2, "Created: %d\n\r", retVal);
-	retVal = Create(0, otherTask);     //instances with higher priority
-	bwprintf(COM2, "Created: %d\n\r", retVal);
-	retVal = Create(0, otherTask);
+
+	retVal = Create(2, otherTask2);	  //instances with lower priority
 	bwprintf(COM2, "Created: %d\n\r", retVal);
 
 	//exit
@@ -133,7 +161,7 @@ void getNextRequest(TD *active, Request *req){
 	// Setup the jump table to branch to kerent on swi
 	void (*syscall)();
 	syscall = &&kerent;
-	*(int *)(0x28) = syscall;
+	*(int *)(0x28) = (int)syscall;
 
 	// 1. push the kernel registers onto its stack
 	asm("stmfd sp!, {r0, r4-r9, sl, fp}");
@@ -166,10 +194,14 @@ void getNextRequest(TD *active, Request *req){
 
 kerent:
 	// 1. acquire the arguments of the request
-	asm("stmfd sp!, {r0-r3}\n\t"
+	// Note: r4 at this point is just garbage.
+	asm("stmfd sp!, {r0-r4}\n\t"
 		"ldr r0, [lr, #-4];\n\t"
 		"bic r1, r0, #0xFF000000\n\t"
 		"stmfd sp!, {r1}");
+	// Used to determine if there are 5 arguments. Otherwise arg4 will be garbage.
+	 asm("ldr r1, [fp, #4]\n\t"
+	 	 "str r1, [sp, #20]");
 
 	// 1.5 get the start of the TDs out from stack
 	// 1.6 get SPSR of the active task into the SP_SVC
@@ -186,7 +218,8 @@ kerent:
 	// 4. overwrite lr with the value from step 2
 	asm("ldmfd r2!, {lr}");
 	// 5. push the registers of the active task onto its stack
-	asm("mov ip, sp\n\t"
+	asm("Store: "
+		"mov ip, sp\n\t"
 		"stmfd sp!, {r4-r9, sl, fp, ip, lr}\n\t"
 		"mov ip, sp");
 	// 7. return to SVC state;
@@ -197,15 +230,16 @@ kerent:
 	// 7.5 get SPSR of the active task from SP_SVC
 	asm("ldmfd sp!, {r1}");
 	// 10. fill in the request of the kernel from its stack
-	asm("ldmfd sp!, {r0,r2-r6}");
-		// asm("mov r0, #1\n\t"
-		// "mov r1, r6\n\t"
-		// "bl bwputr(PLT)");
-	asm("str r2, [r6, #0]\n\t"
-		"str r3, [r6, #4]\n\t"
-		"str r4, [r6, #8]\n\t"
-		"str r5, [r6, #12]\n\t"
-		"str r0, [r6, #16]");
+	asm("ldmfd sp!, {r0,r2-r7}");
+	// asm("mov r0, #1\n\t"
+	// 	"mov r1, r6\n\t"
+	// 	"bl bwputr(PLT)");
+	asm("str r2, [r7, #0]\n\t"
+		"str r3, [r7, #4]\n\t"
+		"str r4, [r7, #8]\n\t"
+		"str r5, [r7, #12]\n\t"
+		"str r6, [r7, #16]\n\t"
+		"str r0, [r7, #20]");
 	// 9. pop the registers from its stack
 	asm("ldmfd sp!, {r0, r4-r9, sl, fp}");
 	// 6. acquire the sp of the active task;
@@ -213,6 +247,18 @@ kerent:
 	asm("str ip, [r0, #0]\n\t"
 		"str r1, [r0, #4]");
 } // getNextRequest
+
+//-----------------------------------------------------------------------------------------------
+//	Same functionality as memcpy().
+//-----------------------------------------------------------------------------------------------
+void copyMsg( char *destBuf, char *srcBuf, int len){
+        int i;
+        for (i = 0; i < len; i++)
+        {
+                destBuf[i] = srcBuf[i];
+        }
+        destBuf[i] = 0;   //Indicate the end of message
+}
 
 //-----------------------------------------------------------------------------------------------
 //	Gets the stack size to use for each user task
@@ -242,7 +288,7 @@ void initialize( TD *tds, Queue *priorityQueues, Request *req ) {
 	}
 
 	// Set stack pointer, PSR, and return value
-	tds[0].sp = MEM_END - getStackSize();
+	tds[0].sp = (int *)MEM_END - getStackSize();
 										// First user stack starts below kernel stack
 	tds[0].spsr = PSR_USR;				// Initialize to user mode
 	tds[0].retVal = 0;					// Initialize return value to 0
@@ -250,11 +296,12 @@ void initialize( TD *tds, Queue *priorityQueues, Request *req ) {
 	tds[0].tid = 1;						// First user task has ID 1
 	tds[0].parentTid = 0;				// Parent is 0 (kernel)
 	tds[0].nextTask = 0;				// No next task in priority queue
+	tds[0].priority = 1;
 
 	// Set lr to the location of the firstUserTask
 	void (*syscall)();
 	syscall = firstUserTask;
-	*(tds[0].sp) = syscall+LOAD_LOC;
+	*(tds[0].sp) = (int)syscall+LOAD_LOC;
 
 	// Fill user stack with placeholder values, and put the pc of firstUserTask into lr
 	for ( i = 12; i > 3; i-- ) {
@@ -296,7 +343,7 @@ void rescheduleActive( Queue *priorityQueues, Request *req ) {
 	unsigned int whichQueue	= req->taskPriority;
 	Queue *currQueue = &(priorityQueues[whichQueue]);
 	TD *lastActive = currQueue->headOfQueue;
-										// Only move to the back if queue has more than 1 task
+	// Only move to the back if queue has more than 1 task
 	if ( currQueue->headOfQueue != currQueue->tailOfQueue ) {
 		currQueue->headOfQueue = (TD *)lastActive->nextTask;
 		currQueue->tailOfQueue->nextTask = (struct TD *)lastActive;
@@ -304,6 +351,36 @@ void rescheduleActive( Queue *priorityQueues, Request *req ) {
 		lastActive->nextTask = 0;
 	} // if
 } // rescheduleActive
+
+//-----------------------------------------------------------------------------------------------
+//	Place a block task at the back of the priority queue that it is in
+//-----------------------------------------------------------------------------------------------
+void rescheduleBlock( Queue *priorityQueues, int priority , TD *blockedTD) {
+	unsigned int whichQueue	= priority;
+	Queue *targetQ = &(priorityQueues[whichQueue]);
+	if(targetQ->headOfQueue == 0){			// empty priority queue
+		targetQ->headOfQueue = blockedTD;
+		targetQ->tailOfQueue = blockedTD;
+	}
+	else {
+		targetQ->tailOfQueue->nextTask = (struct TD *)blockedTD;
+		targetQ->tailOfQueue = blockedTD;
+		blockedTD->nextTask = 0;
+	}
+} // rescheduleActive
+
+//-----------------------------------------------------------------------------------------------
+//	Remove a task descriptor from the priority queues in order to block it
+//-----------------------------------------------------------------------------------------------
+void blockActive(int priority, Queue *priorityQueues){
+	Queue *currQueue = &(priorityQueues[priority]);
+	TD *lastActive = currQueue->headOfQueue;
+	currQueue->headOfQueue = (TD *)lastActive->nextTask;
+}
+
+//-----------------------------------------------------------------------------------------------
+//	Place a block task at the back of the priority queue that it is in
+//-----------------------------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------------------------
 //	Remove the last active task from its priority queue so it will no longer be scheduled
@@ -349,7 +426,7 @@ void handle( TD *tds, Queue *priorityQueues, Request *req ) {
 				syscall = (void *)req->arg1;						// The code of the created task
 
 				// Populate the new TD
-				newTask->sp = MEM_END - newTid * getStackSize();
+				newTask->sp = (int *)MEM_END - newTid * getStackSize();
 																	// Position of new stack in memory
 				newTask->spsr = PSR_USR;							// Initialize to user mode
 				newTask->retVal = 0;								// Initialize return value to 0
@@ -357,9 +434,11 @@ void handle( TD *tds, Queue *priorityQueues, Request *req ) {
 				newTask->tid = newTid;								// First user task has ID 1
 				newTask->parentTid = parentTid;						// Parent is 0 (kernel)
 				newTask->nextTask = 0;								// No next task in priority queue
+				newTask->priority = priority;
+				newTask->nextSender = 0;							// This task has no next sender
 
 				// Set lr to the location of the firstUserTask
-				*(newTask->sp) = syscall+LOAD_LOC;
+				*(newTask->sp) = (int)syscall+LOAD_LOC;
 
 				// Fill user stack with placeholder values, and put the pc of firstUserTask into lr
 				unsigned int i;
@@ -372,7 +451,7 @@ void handle( TD *tds, Queue *priorityQueues, Request *req ) {
 					newQueue->headOfQueue = newTask;
 					newQueue->tailOfQueue = newTask;
 				} else {
-					newQueue->tailOfQueue->nextTask = newTask;
+					newQueue->tailOfQueue->nextTask = (struct TD *)newTask;
 					newQueue->tailOfQueue = newTask;
 				} // if
 
@@ -409,6 +488,90 @@ void handle( TD *tds, Queue *priorityQueues, Request *req ) {
 				closeActive( priorityQueues, req );					// Remove last active task from the queue
 			}
 			break;
+		case SEND:
+			{   /*  Send */
+				// Fill the request arguments into the sender's TD and senderInfo
+				unsigned int whichQueue	= req->taskPriority;		// Priority of last active task
+				unsigned int senderTid 	= priorityQueues[whichQueue].headOfQueue->tid;
+				TD *sender        = &(tds[senderTid - 1]);
+				int receiverTid   = req->arg0;
+				sender->msgToSend = (char *)req->arg1;
+				sender->msgLen    = req->arg2;
+				sender->rcvAddr   = (char *)req->arg3;
+				sender->rcvLen    = req->arg4;
+				TD *receiver      = &(tds[receiverTid - 1]);
+					
+				blockActive(whichQueue, priorityQueues);     //block the current send task 
+
+				// Check if Receive() is called before Send()
+				if (receiver->state == SND_BLOCKED)
+				{   /* Receive() before Send(). Copy the msg to receiver directly */
+					sender->state = RPL_BLOCKED;
+					receiver->state = READY;
+					rescheduleBlock(priorityQueues, receiver->priority, receiver);
+					unsigned int copyLen = (sender->msgLen + 1 < receiver->rcvLen) ? sender->msgLen : (receiver->rcvLen - 1); // send the bottle neck len
+					copyMsg(receiver->rcvAddr, sender->msgToSend, copyLen);
+					*(receiver->senderTid) = senderTid;        // Indicate who the sender is
+				}
+				else { 
+					sender->state = RCV_BLOCKED;
+					sender->nextSender = 0;   // Adding the sender to the back of sendQ
+					if (receiver->nextSender != 0)
+					{
+						TD *lastSender         = (TD *)receiver->lastSender;
+						lastSender->nextSender = (struct TD *)sender;
+						receiver->lastSender   = (struct TD *)sender;
+					}else {
+						receiver->nextSender = (struct TD *)sender;
+						receiver->lastSender = (struct TD *)sender;
+					}
+				}
+			}
+			break;
+		case RECEIVE:
+		    {
+		    	unsigned int whichQueue	 = req->taskPriority;		// Priority of last active task
+				unsigned int receiverTid = priorityQueues[whichQueue].headOfQueue->tid;
+				TD *receiver        = &(tds[receiverTid - 1]);
+				receiver->senderTid = (int *)req->arg0;      // Use to record which task I reply to
+				receiver->rcvAddr   = (char *)req->arg1;     // Place to store sent message
+				receiver->rcvLen    = req->arg2;             // Maximum size of msg I can store
+
+				// Check if sendQ is empty
+				if (receiver->nextSender == 0)
+				{
+					receiver->state = SND_BLOCKED;
+					blockActive(whichQueue, priorityQueues);
+				}
+				else {   // Not empty, copy the msg
+					TD *sender = (TD *)receiver->nextSender;
+					unsigned int copyLen = (sender->msgLen + 1 < receiver->rcvLen) ? sender->msgLen : (receiver->rcvLen - 1); // send the bottle neck len
+					copyMsg(receiver->rcvAddr, sender->msgToSend, copyLen);
+					*(receiver->senderTid) = sender->tid;        // Indicate who the sender is
+					rescheduleActive(priorityQueues, req);
+					// Remove sender from sendQ
+					sender->state = RPL_BLOCKED;
+					receiver->nextSender = sender->nextSender;
+				}
+		    }
+		    break;
+		case REPLY:
+		    {
+				TD *sender   = &(tds[req->arg0 - 1]);
+				char *reply  = (char *) req->arg1;
+				int replyLen = req->arg2;
+
+				unsigned int copyLen = (replyLen + 1 < sender->rcvLen) ? replyLen : (sender->rcvLen - 1); // send the bottle neck len
+				copyMsg(sender->rcvAddr, reply, copyLen);
+
+				// Put both tasks back to ready queue
+				sender->state = READY;
+				rescheduleBlock(priorityQueues, sender->priority, sender);
+				rescheduleActive(priorityQueues, req);
+
+
+		    }
+		    break;
 	} // switch
 } // handle
 
@@ -420,8 +583,8 @@ void handle( TD *tds, Queue *priorityQueues, Request *req ) {
 //-----------------------------------------------------------------------------------------------
 int main( int argc, char *argv[] ) {
 	// Declare kernel data structures
-	TD tds[MAX_TASKS];							// hardcode 33 tasks max
-	Queue priorityQueues[MAX_PRIORITIES];		// hardcode 8 priority queues
+	TD tds[MAX_TASKS];						
+	Queue priorityQueues[MAX_PRIORITIES];
 
 	TD *active;									// The task that will run
 	Request req;								// The requests of the task along with other info
@@ -433,7 +596,7 @@ int main( int argc, char *argv[] ) {
 												// Active will be scheduled to run next
 		if ( active == 0 ) break;				// Return cleanly if all tasks exited
 		getNextRequest( active, &req );			// req is a pointer to a Request
-		handle( tds, priorityQueues, &req );	// Execute the kernel code of the kernel primitive
+		handle( tds, priorityQueues, &req);	// Execute the kernel code of the kernel primitive
 	} // for
 
 	return 0;
--- a2/test/wtA2.h
+++ a2/test/wtA2.h
@@ -8,6 +8,9 @@
 #define MYPARENTTID 	3
 #define PASS 			4
 #define EXIT 			5
+#define SEND            6
+#define RECEIVE         7
+#define REPLY           8
 
 struct TD;
 
@@ -18,9 +21,17 @@ typedef struct {
 	int *sp;
 	int spsr;
 	int retVal;
-	enum { ACTIVE, READY, BLOCKED, ZOMBIE } state;
+	int priority;
+	enum { ACTIVE, READY, SND_BLOCKED, RCV_BLOCKED, RPL_BLOCKED, ZOMBIE } state;
 	unsigned int tid;
 	unsigned int parentTid;
+	void *rcvAddr;
+	int rcvLen;
+	char *msgToSend;
+	int msgLen;
+	int *senderTid;
+	struct TD *nextSender;
+	struct TD *lastSender;
 	struct TD *nextTask;
 } TD;
 
@@ -41,9 +52,11 @@ typedef struct {
 	int arg1;
 	int arg2;
 	int arg3;
+	int arg4;
 	unsigned int type;
 	unsigned int freeIndex;
 	unsigned int taskPriority;
 	TD *tds;
 	Queue *priorityQueues;
 } Request;
+
